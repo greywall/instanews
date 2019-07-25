85fe44db5b521f6cf242386e734ab429ef0f716e
T Grewal <talwindergrewal01@gmail.com>
2019-07-25 12:04:17 -0700

    Add images for each article to page

--- js/main.js
+++ js/main.js
@@ -1,58 +1,63 @@
+$(function() {
+  $("#article-select").on("change", function() {
+    console.log("changed");
+    const selected = $(this).val();
+    if (selected === "") return;
 
-$(function () {
+    loadArticles(selected);
+  });
 
-    $('#article-select').on('change', function () {
-        console.log('changed');
-        const selected = $(this).val();
-        if (selected === '')
-            return;
+  // }).done(function(data){
+  // console.log(data.results);
 
-        loadArticles(selected);
+  //     const results = data.results;
+  //     // $.each()
+  //     //append your article template
+  //     // $.each(results, function(index,value) {
+  //     // try template springs <article> <p>${abstract}</p></article>
+  //     // })
+
+  // }).fail(function(err){
+  //     console.log(err)
+  // }).always(function(){
+  //     console.log('always runs');
+
+  // });
+
+  function loadArticles(selected) {
+    $.ajax({
+      method: "get",
+      url: `https://api.nytimes.com/svc/topstories/v2/${selected}.json?api-key=SuyqPBLEaZeA2ccThYAa6TIJ5jA35kOj`
+    }).done(function(data) {
+      console.log(data);
+      const results = data.results.slice(0, 12);
+
+      $("#article-content").html("");
+
+      $.each(results, function(index, value) {
+        // console.log(value.name);
+
+        const abstract = value.abstract;
+        const imgUrl = value.multimedia[4];
+        console.log(value.multimedia[4]);
+        $("#article-content").append(`<li>
+                        <a href="#">
+                            <div class="grid-cell" id="grid-cell-${index}" style="background-image: url(${value.multimedia[4].url})">
+                                <div>  
+                                <article> 
+                                    <p>${abstract}</p>
+                                        </article>  
+                                        </div>
+                                        </div>
+                                        </a>
+                                        </li>`);
+      });
+      // .fail(function () {
+
+      // })
+      // .always(function () {
+
+      // })
     });
-
-
-
-    // }).done(function(data){
-    // console.log(data.results);
-
-    //     const results = data.results;
-    //     // $.each()
-    //     //append your article template
-    //     // $.each(results, function(index,value) {
-    //     // try template springs <article> <p>${abstract}</p></article>
-    //     // })
-
-    // }).fail(function(err){
-    //     console.log(err)
-    // }).always(function(){
-    //     console.log('always runs');
-
-    // });
-
-    function loadArticles(selected) {
-        $.ajax({
-            method: "get",
-            url: `https://api.nytimes.com/svc/topstories/v2/${selected}.json?api-key=SuyqPBLEaZeA2ccThYAa6TIJ5jA35kOj`
-        })
-            .done(function (data) {
-                console.log(data);
-                const results = data.results.slice(0,12);
-
-                // TRY LOOPING THROUGH data.results
-                $('#article-content').html('');
-
-                $.each(results, function (index, value) {
-                    console.log(value.name);
-                    const abstract = value.abstract;
-                    $('#article-content').append(`<article> <p>${abstract}</p></article>`);
-                });
-                    // .fail(function () {
-
-                    // })
-                    // .always(function () {
-
-                    // })
-            })
-        }
-
-        }); //End of Document Ready
+  }
+}); //End of Document Ready
