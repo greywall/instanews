2e9d50f6176a1e381edd69dec455bb565ea60860
T Grewal <talwindergrewal01@gmail.com>
2019-07-29 16:02:15 -0700

    Add correct height to abstract container

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function() {
   $("#article-select").on("change", function() {
+    $(".loader-container-appear").show();
     console.log("changed");
     const selected = $(this).val();
-    if (selected === "") return;
+    if (selected === "") {
+      $(".loader-container-appear").hide();
+      $("#article-content").html("");
+
+      return;
+    }
 
     loadArticles(selected);
   });
@@ -28,53 +34,60 @@ $(function() {
     $.ajax({
       method: "get",
       url: `https://api.nytimes.com/svc/topstories/v2/${selected}.json?api-key=SuyqPBLEaZeA2ccThYAa6TIJ5jA35kOj`
-    }).done(function(data) {
-      console.log(data);
+    })
+      .done(function(data) {
+        console.log(data);
 
-      const results = data.results;
+        const results = data.results;
 
-      const filteredArray = results.filter(function(article){
-        return article.multimedia[4] !== undefined;
-      }).slice(0, 12);
+        const filteredArray = results
+          .filter(function(article) {
+            return article.multimedia[4] !== undefined;
+          })
+          .slice(0, 12);
 
-      console.log(filteredArray);
+        console.log(filteredArray);
 
-      // const imageArticle = .each(data.results.multimedia[4]).filter(=== "");
-      // const results = data.results.slice(0, 12)};
-     
-      // const results =  filter(data.results.multimedia[4] = ).data.results.slice(0, 12)};
-      
-      
-      // if (data.results.multimedia[4] != "") {
-      //   const results = data.results.slice(0, 12);
-      // }
+        // const imageArticle = .each(data.results.multimedia[4]).filter(=== "");
+        // const results = data.results.slice(0, 12)};
 
-      $("#article-content").html("");
+        // const results =  filter(data.results.multimedia[4] = ).data.results.slice(0, 12)};
 
-      $.each(filteredArray, function(index, value) {
-        // console.log(value.name);
+        // if (data.results.multimedia[4] != "") {
+        //   const results = data.results.slice(0, 12);
+        // }
 
-        const abstract = value.abstract;
-        
-        console.log(value.multimedia[4]);
-        $("#article-content").append(`<li class="article-li">
-                        <a href="#">
-                            <div class="grid-cell" id="grid-cell-${index}" style="background: linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.0)), url(${value.multimedia[4].url}) center/cover">
-                                <div class="article-div">  
-                                <article class="article-child"> 
-                                    <p class="article-abstract">${abstract}</p>
-                                        </article>  
-                                        </div>
-                                        </div>
-                                        </a>
-                                        </li>`);  
+        $("#article-content").html("");
+
+        $.each(filteredArray, function(index, value) {
+          // console.log(value.name);
+
+          const abstract = value.abstract;
+
+          console.log(value.multimedia[4]);
+          $("#article-content").append(
+            `<li class="article-li">
+                <a href="${value.short_url}">
+                <div class="grid-cell" id="grid-cell-${index}" style="background: linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.0)), url(${
+              value.multimedia[4].url
+            }) center/cover">
+                   <div class="article-div">  
+                    <article class="article"> 
+                       <p class="article-abstract">${abstract}</p>
+                    </article>  
+                    </div>
+                   </div>
+                   </a>
+              </li>`
+          );
+        });
+      })
+      .fail(function() {
+        console.log("something is really messed up");
+      })
+      .always(function() {
+        $(".loader-container-appear").hide();
+        console.log("This will always appear after a selection change.");
       });
-      // .fail(function () {
-
-      // })
-      // .always(function () {
-
-      // })
-    });
   }
 }); //End of Document Ready
